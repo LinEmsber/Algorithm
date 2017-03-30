5edff8fd599a716ac70e93c7fdd2deca8786a304
LinEmsber <LinEmsber@gmail.com>
2017-03-30 11:43:21 +0800

    Functionalize algorithm, bugs fix, and test.
    1. convert the algorithm as a function for convenient to use.
    2. add clear explanation.
    3. use valgrind to test.

--- rand_num_generator_02.c
+++ rand_num_generator_02.c
@@ -1,5 +1,7 @@
-/* Random Number Generator, RNG.
- * Using /dev/urandom to generate a random integer number array
+/* Random Number Generator, RNG
+ * Using /dev/urandom to generate a random number with data type uint8_t.
+ *
+ * The maximun length of number array is 255.
  *
  * Reference:
  * http://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)Randomization.html
@@ -7,10 +9,36 @@
  * http://insanecoding.blogspot.tw/2014/05/a-good-idea-with-bad-usage-devurandom.html
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
+/* random number generator, RNG.
+ * generate 8 bits unsigned integer, range from 0 to 255.
+ * This function allocate memory from heap, it is necessary to free it.
+ */
+uint8_t * rand_num_generator_uint8(uint8_t len, uint8_t max, uint8_t min)
+{
+        int i;
+        FILE * f;
+
+        uint8_t rand_val;
+        uint8_t * rand_val_array;
+        uint8_t range = max - min;
+
+        rand_val_array = malloc( len * sizeof(uint8_t) );
+
+        f = fopen("/dev/urandom", "r");
+
+        for (i = 0; i < len; i++){
+                fread(&rand_val, sizeof(rand_val), 1, f);
+                rand_val_array[i] = (rand_val % range) + min;
+        }
+
+        fclose(f);
+
+        return rand_val_array;
+}
 
 void print_arr(uint8_t *arr, int len)
 {
@@ -24,24 +52,14 @@ void print_arr(uint8_t *arr, int len)
 
 int  main(int argc, char *argv[])
 {
-	int i;
-        // int n = atoi()
+        int n = atoi( argv[1] );
+        uint8_t * array;
 
-	FILE *f;
-        uint8_t rand_val;
-	uint8_t rand_val_array[ atoi(argv[1]) ];
+        array = rand_num_generator_uint8(n, 255, 128);
 
-	f = fopen("/dev/urandom", "r");
+        print_arr(array, n);
 
-	for (i = 0; i < atoi(argv[1]); i++){
-		fread(&rand_val, sizeof(rand_val), 1, f);
-		rand_val_array[i] = rand_val;
-	}
+        free(array);
 
-	print_arr(rand_val_array, atoi(argv[1]));
-
-	fclose(f);
-
-
-	return 0;
+        return 0;
 }
