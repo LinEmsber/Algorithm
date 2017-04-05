3675ac6f0a21694452de927f002304f9a4008ea7
Lin Emsber <linemsber@gmail.com>
2017-04-06 00:14:56 +0800

    Add SET_ALL_BITS() and rename all macros.
    1. Add SET_ALL_BITS() to set all bits at one time.
    2. Rename all macros to distinguish to set all bits or some bits at one time.

--- Bitwise/bits_manipulation_01.c
+++ Bitwise/bits_manipulation_01.c
@@ -1,6 +1,8 @@
-/* bitwise manipulation */
-
-// SET_BIT(), CLEAR_BIT(), TOGGLE_BIT(), TEST_BIT()
+/* bits manipulation
+ *
+ * We have following functions to manipulate the bits of a register.
+ * SET_ALL_BITS(), SET_SPECFIC_BIT(), CLEAR_SPECFIC_BIT(), TOGGLE_SPECFIC_BIT(), TEST_SPECFIC_BIT()
+ */
 
 #include <stdio.h>
 
@@ -18,19 +20,27 @@
 	(byte & 0x02 ? '1' : '0'), 	\
 	(byte & 0x01 ? '1' : '0')
 
-
-#define SET_BIT(x, y)           (x |= (y) )
-#define CLEAR_BIT(x, y)         (x &= ~(y) )
-#define TOGGLE_BIT(x, y)        (x ^= (y) )
-#define TEST_BIT(x, y)          (x &= (y) )
+#define SET_ALL_BITS(x, y)		( (x) = (y) )
+#define SET_SPECFIC_BIT(x, y)		( (x) |= (y) )
+#define CLEAR_SPECFIC_BIT(x, y)         ( (x) &= ~(y) )
+#define TOGGLE_SPECFIC_BIT(x, y)        ( (x) ^= (y) )
+#define TEST_SPECFIC_BIT(x, y)          ( (x) &= (y) )
 
 int main()
 {
         int a = 0b00000000;             // original
-        int b = 0b01010101;             // set bits
-        int c = 0b01010000;             // clear bits
-        int d = 0b00001111;             // toggle bits
-        int e = 0b00001111;             // test bits
+	int b = 0b11110000;		// set all bits
+        int c = 0b01010101;             // set specific bits
+        int d = 0b01010000;             // clear specific bits
+        int e = 0b00001111;             // toggle specific bits
+        int f = 0b00001111;             // test specific bits
+
+	printf("#define SET_ALL_BITS(x, y)		( (x) = (y) )\n");
+	printf("#define SET_SPECFIC_BIT(x, y)		( (x) |= (y) )\n");
+	printf("#define CLEAR_SPECFIC_BIT(x, y)         ( (x) &= ~(y) )\n");
+	printf("#define TOGGLE_SPECFIC_BIT(x, y)        ( (x) ^= (y) )\n");
+	printf("#define TEST_SPECFIC_BIT(x, y)          ( (x) &= (y) )\n");
+
 
         printf("\n");
         printf("a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
@@ -38,26 +48,32 @@ int main()
         printf("c: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(c));
         printf("d: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(d));
         printf("e: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(e));
+        printf("f: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(e));
         printf("\n");
 
-        printf("before SET_BIT(a, b), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
-        SET_BIT(a, b);
-        printf("after  SET_BIT(a, b), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+        printf("before SET_ALL_BITS(a, b), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+        SET_ALL_BITS(a, b);
+        printf("after  SET_ALL_BITS(a, b), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
         printf("\n");
 
-        printf("before CLEAR_BIT(a, c), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
-        CLEAR_BIT(a, c);
-        printf("after  CLEAR_BIT(a, c), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+	printf("before SET_SPECFIC_BIT(a, c), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+	SET_SPECFIC_BIT(a, c);
+	printf("after  SET_SPECFIC_BIT(a, c), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+	printf("\n");
+
+        printf("before CLEAR_BIT(a, d), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+        CLEAR_SPECFIC_BIT(a, d);
+        printf("after  CLEAR_BIT(a, d), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
         printf("\n");
 
-        printf("before TOGGLE_BIT(a, d), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
-        TOGGLE_BIT(a, d);
-        printf("after  TOGGLE_BIT(a, d), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+        printf("before TOGGLE_BIT(a, e), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+        TOGGLE_SPECFIC_BIT(a, e);
+        printf("after  TOGGLE_BIT(a, e), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
         printf("\n");
 
-        printf("before TEST_BIT(a, e), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
-        TEST_BIT(a, e);
-        printf("after  TEST_BIT(a, e), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+        printf("before TEST_BIT(a, f), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
+        TEST_SPECFIC_BIT(a, f);
+        printf("after  TEST_BIT(a, f), a: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
         printf("\n");
 
         return 0;
