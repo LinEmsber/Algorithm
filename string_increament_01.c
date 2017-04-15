1d0a7e16691fc16e3849342a0865d5a1a884f1b1
Lin Emsber <linemsber@gmail.com>
2017-04-15 20:52:50 +0800

    A simple algorithm of string increment.

---
+++ string_increament_01.c
@@ -0,0 +1,24 @@
+/* The simple algorithm for string increament. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+	int i, j;
+	char string[8] = "ABCDEFG";
+
+	for (i = 0; i < 10000; i++){
+
+		for (j = 0; j < 7; j++){
+			string[j] ++;
+			string[j] = ( (string[j] - 65) % 52) + 65;
+		}
+
+		printf("string: %s\n", string);
+	}
+
+	return 0;	
+}
