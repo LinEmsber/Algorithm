7a83574490e245871ec90c487d597fbecd440799
Lin Emsber <linemsber@gmail.com>
2017-05-17 22:26:32 +0800

    Review the algorithm of the KMP's failure function.
    1. Using GDB to trace the algorithm.

--- search/KMP_failure_01.c
+++ search/KMP_failure_01.c
@@ -1,9 +1,11 @@
-/* The failure function of KMP algorithm. */
+/* The failure function of KMP algorithm.
+ *
+ * GDB: gcc -Wall -g KMP_failure_01.c && gdbtui ./a.out
+ */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-#define MAX_SIZE  100
 
 /* make Partial match table, also known as failure function */
 int * failure(char * word, int word_size)
@@ -19,10 +21,13 @@ int * failure(char * word, int word_size)
 
 	for (i = 1; i < word_size; i++) {
 
-		while (k > -1 && word[k+1] != word[i])
+		/* If the k is not -1, but the word[i] is not equal the next char (word[k+1]).
+		 * Thus, we reset the k as pi[k].
+		 */
+		while ( k > -1 && word[i] != word[k+1] )
 			k = pi[k];
 
-		if (word[i] == word[k+1])
+		if ( word[i] == word[k+1] )
 			k++;
 
 		pi[i] = k;
@@ -34,12 +39,14 @@ int * failure(char * word, int word_size)
 int main()
 {
 	int i;
-	char * word = "ABACABABC";
+	// char * word = "ABACABABC";
+	char * word = "ABCABCABOOBCDCC";
+
+	int * fail;
 
 	int word_length = strlen(word);
-	int fail[word_length];
 
-	failure(word, fail);
+	fail = failure(word, word_length);
 	printf("word: %s\n", word);
 
 	for (i = 0; i < word_length; i++)
@@ -47,4 +54,3 @@ int main()
 
 	return 0;
 }
-
