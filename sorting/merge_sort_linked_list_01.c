f2527b78791eecedd617ee0075a49bbbe4f31a83
Lin Emsber <linemsber@gmail.com>
2018-09-30 23:03:30 +0800

    Complete the simple merge sort linked list

--- sorting/merge_sort_linked_list_01.c
+++ sorting/merge_sort_linked_list_01.c
@@ -1,268 +1,143 @@
-/* Merge sort in C */
+/* Merge sort the elements of the linked list.
+ * Input a linked list to this function and use merge sort to sort the elements of list.
+ */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/* Linked list node. */
-typedef struct node node_t;
-struct node{
-	int key;
-	void * data;
+struct node
+{
+	int data;
 	struct node * next;
 };
 
-/* Linked list. */
+/* Prototypes */
+void merge_sort_linked_list(struct node ** root);
+struct node * merge_sorted_list_1(struct node * node_a, struct node * node_b);
+void node_move(struct node ** dest_node, struct node ** src_node);
+void split_list(struct node * root_src, struct node ** front_list_ptr, struct node ** back_root_ptr);
 
-node_t * node_create(int value);
-int node_insert_front(node_t * head, node_t * n);
-int node_delete(node_t * head, int target_val);
-
-node_t * node_create(int value)
+/* sorts the linked list. */
+void merge_sort_linked_list(struct node ** root_src)
 {
-	node_t * n;
-	n = malloc( sizeof(*n) );
-	if (n == NULL)
-		return NULL;
+	struct node * root = *root_src;
+	struct node * list_a;
+	struct node * list_b;
 
-	n -> key = value;
-	n -> next = NULL;
-	return n;
+	if ( (root == NULL) || (root->next == NULL) )
+		return;
+
+	/* Split root into 'list_a' and 'list_b' sublists */
+	split_list(root, &list_a, &list_b);
+
+	/* Recursively sort the sublists */
+	merge_sort_linked_list(&list_a);
+	merge_sort_linked_list(&list_b);
+
+	/* answer = merge the two sorted lists together */
+	*root_src = merge_sorted_list_1(list_a, list_b);
 }
 
-int node_insert_front(node_t * head, node_t * n)
+/* Takes two lists sorted in increasing order. */
+struct node * merge_sorted_list_1(struct node * node_a, struct node * node_b)
 {
-	if (head == NULL || n == NULL)
-		return -1;
+	struct node * result_root = NULL;
+	struct node ** root_ref_ptr = &result_root;
 
-	n -> next = head -> next;
-	head -> next = n;
-
-	return 0;
-}
-
-int node_delete(node_t * head, int target_key)
-{
-	int ret = -1;
-	if (head == NULL)
-		return ret;
-
-	node_t **pp = &head;
-	node_t *p = head;
-
-	while(p){
-		if(p->key == target_key){
-			*pp = p->next;
-			free(p);
-			ret = 0;
-
-			/* Remember to break from the while loop. */
+	while(1) {
+		if (node_a == NULL) {
+			*root_ref_ptr = node_b;
+			break;
+		} else if (node_b == NULL) {
+			*root_ref_ptr = node_a;
 			break;
 		}
 
-		pp = &p->next;
-		p = p->next;
+		if(node_a->data <= node_b->data) {
+			node_move(root_ref_ptr, &node_a);
+		} else {
+			node_move(root_ref_ptr, &node_b);
+		}
+
+		root_ref_ptr = &((*root_ref_ptr)->next);
+	}
+	return result_root;
+}
+
+/* Takes the node from the front of the source, and move it to the front of the dest. */
+void node_move(struct node ** dest_node, struct node ** src_node)
+{
+	struct node * tmp_node = *src_node;
+
+	*src_node = tmp_node->next;
+	tmp_node->next = *dest_node;
+	*dest_node = tmp_node;
+}
+
+
+/* Split the nodes of the given list into front and back halves and return the two lists
+ * using the reference parameters.
+ */
+void split_list(struct node * root_src, struct node ** front_list_ptr, struct node ** back_root_ptr)
+{
+	struct node * slow = root_src;
+	struct node * fast = root_src->next;
+
+	/* The fast moves two nodes once a time and the slow moves one node once a time. */
+	while (fast != NULL) {
+		fast = fast->next;
+		if (fast != NULL) {
+			slow = slow->next;
+			fast = fast->next;
+		}
 	}
 
-	return ret;
+	/* The slow is before the middle point. */
+	*front_list_ptr = root_src;
+	*back_root_ptr = slow->next;
+	slow->next = NULL;
 }
 
-node_t * previous_node(node_t * head, node_t * target_node)
+void push(struct node ** head_ref, int new_data)
 {
-	if(head == target_node || head == NULL)
-		return NULL;
+	struct node * new_node = (struct node *) malloc( sizeof(struct node) );
 
-	/* Start from the head node to find the target nodes's previous node. */
-	node_t * current = head;
-	node_t * prev = NULL;
+	new_node->data = new_data;
+	new_node->next = (*head_ref);
 
-	while(current){
-
-		/* Find the target node */
-		if (current == target_node)
-			break;
-
-		prev = current;
-		current = current->next;
-	}
-
-	/* Return target nodes's previous node. */
-	return prev;
+	*head_ref = new_node;
 }
 
-int swap_nodes(node_t ** head, node_t ** node_1, node_t ** node_2)
+void print_list(struct node * node)
 {
-	if (head == NULL|| node_1 == NULL || node_2 == NULL)
-		return -1;
-
-	node_t * first = *node_1;
-	node_t * second = *node_2;
-	node_t * first_prev, * second_prev;
-	node_t * tmp = NULL;
-
-	/* Find the target nodes's previous node. */
-	first_prev = previous_node(*head, first);
-	second_prev = previous_node(*head, second);
-
-	/* Swap the previous nodes of both nodes. */
-	if(first_prev)
-		first_prev->next = second;
-	else
-		*head = *node_2;
-
-	if(second_prev)
-		second_prev->next = first;
-	else
-		*head = *node_1;
-
-	/* Swap the next nodes of both nodes */
-	tmp = first->next;
-	first->next = second->next;
-	second->next = tmp;
-
-	return 0;
-}
-
-/* Print the list. */
-int print_list_key(node_t * head)
-{
-	if (head == NULL)
-		return -1;
-
-	node_t * current = head;
-
-	while ( current != NULL ){
-		printf("%d ", current->key);
-		current = current -> next;
+	while (node != NULL) {
+		printf("%d ", node->data);
+		node = node->next;
 	}
 	printf("\n");
-
-	return 0;
-}
-
-
-
-
-
-/* merge sort linked list. */
-void merge_sort_linked_list(node_t * head)
-{
-	node_t * root = head;
-	node_t * front_list, * end_list;
-
-	// split list into two parts
-	split_list(root, front_list, end_list);
-
-	// merge sort list recursively
-	merge_sort_linked_list(front_list);
-	merge_sort_linked_list(end_list);
-
-	// merge
-	merge(front_list, end_list);
-}
-
-/* Merge sort. */
-void merge_sort(int *, int);
-void sort(int *, int *, int, int);
-void merge(int *, int*, int, int, int);
-
-/* Create a temp array to store the values of sorted data during time of sorting process */
-void merge_sort(int arr[], int size)
-{
-	/* Create an auxilary array to store sorted data. */
-	int *tmp = (int *)malloc(sizeof(int) * size);
-	
-	/* Call the function to sort data. */
-	sort(arr, tmp, 0, size - 1);
-}
-
-
-/* It divides the problem into subproblems and solve them seperately. 
- * Finally, it combines the data of two subproblems by calling merge().
- */
-void sort(int arr[], int tmp[], int start, int end)
-{
-	/* Divide into subproblems. */
-	if (start < end) {
-		int mid;
-		// calculating the mid vaue
-		mid = start + (end - start) / 2;
-
-		//sorting part 1
-		sort(arr, tmp, start, mid);
-
-		//sorting part 2
-		sort(arr, tmp, mid + 1, end);
-
-		//combining the two sorted parts
-		merge(arr, tmp, start, mid+1, end);
-	}
-}
-
-/* It combines the two subproblems. 
- * It is the conquer part of divide and conquer algorithm.
- */
-void merge(int arr[], int tmp[], int start, int mid, int end)
-{
-	int i, j, k;
-	i = start;	/* The first subpart. */
-	j = mid;	/* The second subpart. */
-	k = start;	/* The temporary array. */
-
-	/* runs till one of the subarray gets the last element. */
-	while ((i < mid) && (j <= end)) {
-		if (arr[i] <= arr[j]) {
-			tmp[k++] = arr[i++];
-		} else {
-			tmp[k++] = arr[j++];
-		}
-	}
-
-	/* If j was the last element, j > end, elements are still left in the first subpart. */
-	while (i < mid) {
-		tmp[k++] = arr[i++];
-	}
-
-	/* If i was the last element, i == mid, elements are still left in the second subpart. */
-	while (j <= end) {
-		tmp[k++] = arr[j++];
-	}
-
-	/* Copy back. */
-	for (i = start; i <= end; i++) {
-		arr[i] = tmp[i];
-	}
 }
 
 int main()
 {
-        int i;
-        int array_len;
-        int arr[] = {10, 2, 4, 5, 22, 98, 84, 12, 13, 31, 56,
-			310, 302, 604, 505, 322, 388, 684, 312, 613, 331, 656,
-			412, 208, 302, 302, 482, 378, 482, 318, 412, 238, 252,
-			212, 102, 602, 284, 372, 128, 672, 284, 212, 131, 152,
-			412, 408, 182, 375, 422, 498, 122, 372, 182, 432, 652,
-			314, 384, 674, 523, 324, 394, 684, 323, 474, 388, 354,
-			483, 378, 323, 305, 423, 298, 383, 312, 323, 371, 683,
-			373, 123, 603, 205, 623, 193, 683, 212, 413, 128, 673,
-			420, 408, 404, 108, 422, 498, 184, 112, 413, 438, 426};
+	struct node * list_a = NULL;
 
-        array_len = sizeof(arr) / sizeof(arr[0]);
+	/* Push the contents to the list. */
+	push(&list_a, 15);
+	push(&list_a, 100);
+	push(&list_a, 35);
+	push(&list_a, 20);
+	push(&list_a, 30);
+	push(&list_a, 85);
+	push(&list_a, 70);
+	push(&list_a, 22);
 
-        /* Before. */
-        printf("Origin: \n");
-	for(i = 0; i < array_len; i++)
-		printf("%d ",arr[i]);
-	printf("\n");
+	printf("Before merge sort, the list: \n");
+	print_list(list_a);
 
-        /* Merge sort. */
-	merge_sort(arr, array_len);
+	merge_sort_linked_list(&list_a);
 
-        /* After. */
-        printf("Merge sort: \n");
-	for(i = 0; i < array_len; i++)
-		printf("%d ",arr[i]);
-	printf("\n");
+	printf("After merge sort, the list: \n");
+	print_list(list_a);
 
 	return 0;
 }
